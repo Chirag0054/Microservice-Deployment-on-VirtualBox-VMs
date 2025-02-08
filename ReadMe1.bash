47231e42748ad2a092e81cea95cf128ea9b41766
Chirag0054 <m23aid005@iitj.ac.in>
2025-02-08 21:48:06 +0530

    Add files via upload

---
+++ ReadMe1.bash
@@ -0,0 +1,7 @@
+#In Both the VMs 
+
+sudo apt update
+sudo apt install python3-pip
+pip3 install flask
+
+nano sudo app.py
---
+++ ReadMe2.bash
@@ -0,0 +1,6 @@
+#Run this command in VM1
+python3 app.py
+
+#Run this command in VM2
+curl http://192.168.1.101:6000
+  
